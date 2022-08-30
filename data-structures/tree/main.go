b38d8b9cf708be06f560afcc9faf494f82fe38ac
John Roberts <john.calvin.roberts@gmail.com>
2022-08-29 23:34:01 -0500

    Fix tree

--- data-structures/tree/main.go
+++ data-structures/tree/main.go
@@ -1,39 +1,68 @@
 package main
 
+import "fmt"
+
 /*
 	Implements a tree using generics in go
 */
 
-type Node[T cancompare] struct {
+type orderable interface {
+	~int8 | ~int16 | ~int32 | ~float32 | ~float64
+}
+type Node[T orderable] struct {
 	val   T
 	left  *Node[T]
 	right *Node[T]
 }
 
-func (n Node[T]) addChild(val T) {
-	if T.compare(val) {
+func (n Node[T]) insert(val T) {
+	defer func() {
+		n.print()
+		fmt.Println("-")
+	}()
+	if val > n.val {
+		x := n.val
+		n.val = val
+		n.insert(x)
+	}
+
+	if n.right == nil {
 		n.right = &Node[T]{val: val}
-	} else {
+		return
+	}
+
+	if n.left == nil && val < n.right.val {
 		n.left = &Node[T]{val: val}
 	}
+
+	if n.right != nil && n.right.val == val {
+		// node already exists on right side
+		return
+	}
+
+	if (n.right != nil && val > n.right.val) || (n.left != nil && val > n.left.val) {
+		n.right.insert(val)
+		return
+	}
+
 }
 
-type cancompare interface {
-	compare() bool
-}
+func (n Node[T]) print() {
+	fmt.Printf("n: %v, left: %v, right: %v\n", n.val, n.left, n.right)
+	if n.right != nil {
+		n.right.print()
+	}
 
-type IntNode struct{}
+	if n.left != nil {
+		n.left.print()
+	}
 
-func (i IntNode) compare(x int, y int) bool {
-	return x > y
-}
-
-type StringNode struct{}
-
-func (s StringNode) compare(x string, y string) bool {
-	return len(x) > len(y)
 }
 
 func main() {
-	// TODO
+	t := &Node[int8]{val: 11}
+	t.insert(12)
+	t.insert(9)
+	t.insert(10)
+	t.print()
 }
